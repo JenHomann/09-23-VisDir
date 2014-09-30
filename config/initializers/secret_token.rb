76b2341535ae83aa083c718513809fea1594ee76
“Jen <mpjeno@gmail.com>
2014-09-30 17:33:31 -0500

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisDir::Application.config.secret_token = '9333fc3e3cc26e0ce5b46d41aa8a047c3d54addea4770fe58f999da7119e566ceb5296d8b1c8a7e9d18abc9c5360dec82077320df2eea146930b207ae7fc3a85'
