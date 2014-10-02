ad2091c22bdfaf63cce39c15e75be5bc08969e03
“Jen <mpjeno@gmail.com>
2014-10-02 00:30:22 -0500

    WIP

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,21 +6,42 @@ class ListingsController < ApplicationController
   end
   
   def new
+    @listings = Listing.all
+    @categories = Category.all
+    @listing = Listing.new
   end
   
   def create
+    @listing = Listing.new(params[:listing])
+    
+    if @listing.save
+      redirect_to listing_path(@listing.id), :notice => "Creating listing."
+    else
+      render "new"
+    end
+    
   end
   
   def edit
+    @listing = Listing.find(params[:id])
   end
   
   def update
+    @listing = Listing.find(params[:id])
+    
+    if @listing.update_attributes(params[:id])
+      redirect_to listing_path(@listing.id), :notice => "Updating listing."
+    else
+      render "edit"
+    end
   end
   
   def show
+    @listing = Listing.find(params[:id])
   end
   
   def destroy
+    @listing = Listing.find(params[:id])
   end
 
 end
