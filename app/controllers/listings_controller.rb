df20b3d233e49d9e1e3c7b74add43f71830b0c60
“Jen <mpjeno@gmail.com>
2014-10-02 00:08:17 -0500

    Matching erb code to Sumeet's html

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,26 @@
 class ListingsController < ApplicationController
+  
+  def index
+    @listings = Listing.all
+    @categories = Category.all
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+
 end
